1cb86f06ee1fa341017d794668e5ba91d9b7a1f2
katguerin <katguerin55@gmail.com>
2020-04-15 16:38:15 -0700

    finished 6.1 moving onto 6.2

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,14 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
+var getUserRepos = function(user) {
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
+
+var response = fetch("https://api.github.com/users/octocat/repos");
+  console.log(response);
+   
   getUserRepos();
